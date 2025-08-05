da658e2c0382966cb433a57bd43384c066748e52
LAPTOP-F6UL31IH\hoang <hoang0109sa@gmail.com>
2025-08-05 08:56:23 +0700

    fix: sword animation, player flip with sword anomation feat: sword follow mouse, sword effect

--- Assets/Script/Sword.cs
+++ Assets/Script/Sword.cs
@@ -4,9 +4,13 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator animator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         playerControls = new PlayerControls();
         animator = GetComponent<Animator>();
     }
@@ -21,8 +25,30 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update()
+    {
+        MouseFollowWithOfset();
+    }
+
     private void Attack()
     {
         animator.SetTrigger("Attack");
     }
+
+    private void MouseFollowWithOfset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180,angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
 }
